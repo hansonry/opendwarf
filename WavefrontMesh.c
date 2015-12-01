d9a280ca28de1214772c1ea92b441713ee0919eb
Ryan Hanson <hansonry@gmail.com>
2015-12-01 09:40:28 -0600

    Now I am stuck

--- WavefrontMesh.c
+++ WavefrontMesh.c
@@ -40,17 +40,21 @@ static WMMeshTexture_T * WavefrontMesh_LookupWMMeshTexture(ObjectList_T * obj_li
 
 static void WavefrontMesh_Convert(WavefrontMesh_T * wmesh, WavefrontLoaderData_T * obj_data, const char * image_file_prefix)
 {
-   size_t i;
+   size_t i, k;
+   size_t count;
+   size_t vertex_count;
+   size_t face_vertex_index;
    size_t material_index;
    char * filename;
    const unsigned int mesh_sizes[3] = {3, 3, 2};
    float vertex[8];
    ManagerGLTexture2D_T * text_manager;
-   WMMeshTexture_T * mesh_texture;
+   WMMeshTexture_T * mesh_texture, ** mt_list;
+   const WavefrontLoaderFaceVertex_T * vert;
    
    text_manager = Resources_GetTextureManager();
 
-   
+   vertex_count = 0;
    for(i = 0; i < obj_data->face_list_count; i++)
    {
       material_index = obj_data->face_list[i].material_index;
@@ -68,15 +72,38 @@ static void WavefrontMesh_Convert(WavefrontMesh_T * wmesh, WavefrontLoaderData_T
 
       }
 
-      // Add Vertex
+      // Add Face
+      for(k = 0; k < 3; k ++)
+      {
+         face_vertex_index = obj_data->face_list[i].face_vertex_start_index + k;
+         vert = &obj_data->face_vertex_list[face_vertex_index];
+         vertex[0] = obj_data->vertex_list[vert->vertex_index].x;
+         vertex[1] = obj_data->vertex_list[vert->vertex_index].y;
+         vertex[2] = obj_data->vertex_list[vert->vertex_index].z;
+         vertex[3] = obj_data->normal_list[vert->normal_index].x;
+         vertex[4] = obj_data->normal_list[vert->normal_index].y;
+         vertex[5] = obj_data->normal_list[vert->normal_index].z;
+         vertex[6] = obj_data->uv_list[vert->uv_index].u;
+         vertex[7] = obj_data->uv_list[vert->uv_index].v;
+
+         GLMeshBuilder_Add1Vertex(&mesh_texture->mesh_builder, vertex);
+
+         GLMeshBuilder_Add1Element(&mesh_texture->mesh_builder, vertex_count);
+         vertex_count++;
+         
+         
+      }
 
    }
 
-
-
-
-
    // One Mesh for each material
+   mt_list = ObjectList_Get(&wmesh->mesh_texture_list, &count);
+   for(i = 0; i < count; i ++)
+   {
+      GLMeshBuilder_CreateGLMesh(&mt_list[i]->mesh_builder, &mt_list[i]->mesh);
+      GLMesh_MoveToGFXCard(&mt_list[i]->mesh);
+      GLMeshBuilder_Destroy(&mt_list[i]->mesh_builder);
+   }
 
 
 }
@@ -106,5 +133,15 @@ void WavefrontMesh_Destroy(WavefrontMesh_T * wmesh)
 
 void WavefrontMesh_Render(WavefrontMesh_T * wmesh)
 {
+   WMMeshTexture_T ** mt_list;
+   size_t i, count;
+
+   mt_list = ObjectList_Get(&wmesh->mesh_texture_list, &count);
+   for(i = 0; i < count; i ++)
+   {
+      GLTexture2D_ApplyToUniform(mt_list[i]->texture);
+      GLMesh_Render(&mt_list[i]->mesh, GL_TRIANGLES);
+   }
+
 }
 
