b75dcd0e417304d8372a922b629c745851c37f63
Ryan Hanson <hansonry@gmail.com>
2015-12-13 10:23:47 -0600

    Attempt to get Pawns to work on their own

--- Pawn.c
+++ Pawn.c
@@ -2,6 +2,7 @@
 
 #include "AStar.h"
 #include <stdlib.h>
+#include <string.h>
 
 static const Position_T plist[4] =
 {
@@ -19,6 +20,10 @@ Pawn_T * Pawn_Create(MapChunk_T * map)
    size_t count;
    pawn = malloc(sizeof(Pawn_T));
    pawn->map = map;
+   pawn->cmd_list = NULL;
+   pawn->cmd_list_count = 0;
+   pawn->cmd_index = 0;
+   pawn->cmd_state = e_PCS_NoCommand;
    Position_Set(&pawn->pos, 4, 2, 4);
    Mover_Init(&pawn->mover, &pawn->pos, 1.0f);
    MoverControllerList_Init(&pawn->mover_ctrl, &pawn->mover, 1);
@@ -48,8 +53,50 @@ void Pawn_Destroy(Pawn_T * pawn)
    free(pawn);
 }
 
+void Pawn_SetComandList(Pawn_T * pawn, PawnCmd_T * cmd_list, size_t cmd_list_count)
+{
+   pawn->cmd_list = cmd_list;
+   pawn->cmd_list_count = cmd_list_count;
+   pawn->cmd_index = 0;
+  
+   if(pawn->cmd_state == e_PCS_NoCommand)
+   {
+      pawn->cmd_state == e_PCS_FinishedCommand;
+   }
+
+}
+
+static void Pawn_InitCurrentCommand(Pawn_T * pawn)
+{
+   pawn->cmd_state = e_PCS_RunningCommand;
+
+}
+
+static Pawn_UpdateCurrentCommand(Pawn_T * pawn)
+{
+}
+
+
+static void Pawn_UpdateCommand(Pawn_T * pawn, float seconds)
+{
+   if(pawn->cmd_state == e_PCS_NoCommand && 
+      pawn->cmd_list != NULL && 
+      pawn->cmd_index < pawn->cmd_list_count)
+   {
+      memcpy(&pawn->current_command, &pawn->cmd_list[pawn->cmd_index], sizeof(PawnCmd_T));
+      Pawn_InitCurrentCommand(pawn);
+   }
+
+   if(pawn->cmd_index == e_PCS_RunningCommand)
+   {
+      Pawn_UpdateCurrentCommand(pawn);
+   }
+
+}
+
 void Pawn_Update(Pawn_T * pawn, float seconds)
 {
+   Pawn_UpdateCommand(pawn, seconds);
    Mover_Update(&pawn->mover, seconds);
    MoverControllerList_Check(&pawn->mover_ctrl);
 }
--- Pawn.h
+++ Pawn.h
@@ -6,6 +6,32 @@
 #include "MapChunk.h"
 
 typedef struct Pawn_S          Pawn_T;
+typedef struct PawnCmd_S       PawnCmd_T;
+typedef enum   PawnCmdType_E   PawnCmdType_T;
+typedef enum   PawnCmdState_E  PawnCmdState_T;
+
+enum PawnCmdState_E
+{
+   e_PCS_NoCommand,
+   e_PCS_RunningCommand,
+   e_PCS_FinishedCommand
+};
+
+enum PawnCmdType_E
+{
+   e_PCT_Wait,
+   e_PCT_MoveTo,
+   e_PCT_PickUp,
+   e_PCT_Drop,
+   e_PCT_Last
+};
+
+struct PawnCmd_S
+{
+   PawnCmdType_T type;
+   Position_T    pos;
+};
+
 
 
 struct Pawn_S
@@ -15,11 +41,17 @@ struct Pawn_S
    Position_T * path_list;
    MoverControllerList_T mover_ctrl;
    MapChunk_T * map;
+   PawnCmd_T * cmd_list;
+   PawnCmd_T   current_command;
+   size_t cmd_list_count;
+   size_t cmd_index;
+   PawnCmdState_T cmd_state;
 };
 
 Pawn_T * Pawn_Create(MapChunk_T * map);
 void Pawn_Destroy(Pawn_T * pawn);
 
+void Pawn_SetComandList(Pawn_T * pawn, PawnCmd_T * cmd_list, size_t cmd_list_count);
 
 void Pawn_Update(Pawn_T * pawn, float seconds);
 #endif // __PAWN_H__
