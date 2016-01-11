7f13aad507f98f83924423c9aac28919e7df8af1
Ryan Hanson <hansonry@gmail.com>
2016-01-10 19:14:30 -0600

    Started working JobManager to make sure things work

--- JobManager.c
+++ JobManager.c
@@ -1,23 +1,44 @@
 #include "JobManager.h"
 
 
-void JobManager_Init(JobManager_T * manager)
+void JobManager_Init(JobManager_T * manager, PawnList_T * pawn_list, 
+                                             MapItemList_T * map_item_list, 
+                                             StockPileList_T * stockpile_list)
 {
-   ObjectList_Init(&manager->pawn_list);
+   manager->pawn_list      = pawn_list;
+   manager->map_item_list  = map_item_list;
+   manager->stockpile_list = stockpile_list;
 }
 
 void JobManager_Destroy(JobManager_T * manager)
 {
-   ObjectList_Destory(&manager->pawn_list);
 }
 
 void JobManager_PawnLooking(JobManager_T * manager, Pawn_T * pawn)
 {
-   ObjectList_Add(&manager->pawn_list, pawn);
 }
 
 void JobManager_PawnNotLooking(JobManager_T * manager, Pawn_T * pawn)
 {
-   ObjectList_Remove(&manager->pawn_list, pawn);
 }
+
+void JobManager_Update(JobManager_T * manager, float seconds)
+{
+   size_t i, k, map_item_count, stockpile_count;
+   MapItem_T * map_item_list;
+   Position_T * stockpile_list;
+
+   // Find stockpiles and items that are not satified 
+   map_item_list  = ListMemory_Get(&manager->map_item_list->mapitem_list, &map_item_count,  NULL);
+   stockpile_list = ListMemory_Get(&manager->stockpile_list->list,        &stockpile_count, NULL);
+
+
+   for(i = 0; i < map_item_count; i++)
+   {
+
+   }
+
+}
+
+
 
--- JobManager.h
+++ JobManager.h
@@ -1,23 +1,25 @@
 #ifndef __JOBMANAGER_H__
 #define __JOBMANAGER_H__
-#include "Pawn.h"
-#include "Item.h"
-#include "ObjectList.h"
+#include "PawnList.h"
+#include "MapItemList.h"
+#include "StockPileList.h"
 
 typedef struct JobManager_S JobManager_T;
 
 struct JobManager_S
 {
-   ObjectList_T pawn_list;
+   StockPileList_T * stockpile_list;
+   MapItemList_T * map_item_list;
+   PawnList_T * pawn_list;
 };
 
-void JobManager_Init(JobManager_T * manager);
+void JobManager_Init(JobManager_T * manager, PawnList_T * pawn_list, MapItemList_T * map_item_list, StockPileList_T * stockpile_list);
 void JobManager_Destroy(JobManager_T * manager);
 
 void JobManager_PawnLooking(JobManager_T * manager, Pawn_T * pawn);
 void JobManager_PawnNotLooking(JobManager_T * manager, Pawn_T * pawn);
 
-
+void JobManager_Update(JobManager_T * manager, float seconds);
 
 
 #endif // __JOBMANAGER_H__
